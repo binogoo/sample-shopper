ba633446de0807e184b7673235e52f7b785002d5
Pooya <pooya@MacBook-Air.local>
2018-03-17 15:18:11 +0330

    Changes for section-01

---
+++ sample-shopper/view/customTextField.swift
@@ -0,0 +1,33 @@
+//
+//  customTextField.swift
+//  sample-shopper
+//
+//  Created by Pooya on 12/26/1396 AP.
+//  Copyright © 1396 Binogoo. All rights reserved.
+//
+
+import UIKit
+
+class customTextField: UITextField {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textAlignment = .center
+        layer.cornerRadius = 5.0
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        if let placeholderValue = placeholder{
+            let place = NSAttributedString(string: placeholderValue,
+                                           attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) ])
+            
+            attributedPlaceholder = place
+            
+        }
+        
+        
+    }
+    
+
+}
