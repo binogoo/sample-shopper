a5f740ea21757415864dbb910fad87dd9f6d75aa
Pooya <pooya@MacBook-Air.local>
2018-03-21 15:04:44 +0330

    Currency symbol is added to the custom text field

--- sample-shopper/view/customTextField.swift
+++ sample-shopper/view/customTextField.swift
@@ -11,7 +11,26 @@ import UIKit
 @IBDesignable
 class customTextField: UITextField {
 
-    
+
+    override func draw(_ rect: CGRect) {
+        
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: frame.size.height/2 - size/2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.6791791524)
+        currencyLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        currencyLabel.textAlignment = .center
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        
+        addSubview(currencyLabel)
+        
+        
+    }
     
     override func prepareForInterfaceBuilder() {
         createCustomView()
@@ -28,6 +47,8 @@ class customTextField: UITextField {
         layer.cornerRadius = 5.0
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        clipsToBounds = true
+        
         
         if let placeholderValue = placeholder{
             let place = NSAttributedString(string: placeholderValue,
